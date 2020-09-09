1b24ecf4ccbeb86a759603a6b0b801e8f253df0a
ruthjomo <ruthjomo19@gmail.com>
2020-09-09 09:21:03 +0300

    changed codes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProfileComponent } from './profile/profile.component';
+import { ReposComponent } from './repos/repos.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'profile', component: ProfileComponent},
+  { path: 'repos', component: ReposComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
